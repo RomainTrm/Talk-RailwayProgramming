532affdad2a29db925b4a3fcf9a6c96b4b42c096
Romain BERTHON <>
2022-11-20 15:58:16 +0100

    Talk: improve live coding

--- TalkRailwayProgramming/Talk/Result.cs
+++ TalkRailwayProgramming/Talk/Result.cs
@@ -5,6 +5,12 @@ public abstract record Result<TValue, TError>
     public abstract TResult Match<TResult>(
         Func<TValue, TResult> ok,
         Func<TError, TResult> error);
+
+    // public abstract Result<TResult, TError> Map<TResult>(
+    //     Func<TValue, TResult> morphism);
+    //
+    // public abstract Result<TResult, TError> Bind<TResult>(
+    //     Func<TValue, Result<TResult, TError>> morphism);
 }
 
 public sealed record Ok<TValue, TError>(TValue Value) : Result<TValue, TError>
@@ -21,24 +27,4 @@ public sealed record Error<TValue, TError>(TError Value) : Result<TValue, TError
         Func<TValue, TResult> ok,
         Func<TError, TResult> error)
         => error(Value);
-}
-
-
-
-
-
-
-
-
-public static class ResultExtensions
-{
-    public static Result<TResult, TError> Map<TValue, TResult,TError>(
-        this Result<TValue, TError> result,
-        Func<TValue, TResult> morphism)
-        => throw new System.NotImplementedException();
-
-    public static Result<TResult, TError> Bind<TValue, TResult,TError>(
-        this Result<TValue, TError> result,
-        Func<TValue, Result<TResult, TError>> morphism)
-        => throw new System.NotImplementedException();
 }
